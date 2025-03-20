3cfedda20cc19fae129511865e3200c8dbab1966
Letícia Fernandes <104858001+lele-sf@users.noreply.github.com>
2025-03-19 22:22:33 -0300

    Add gradient comp
    * Initialize project with Expo setup, add main application files and assets
    
    * Add GradientText component and implement custom font styling in App.js

---
+++ styles/globalStyles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+
+export const colors = {
+};
+
+export const globalStyles = StyleSheet.create({
+    font: {
+        fontFamily: "MavenPro_400Regular",
+    },
+});
+  // COLORS:
+  //
+  // principalText: {
+  //   color: '#E5E5E5',
+  // },
+  // principalBox: {
+  //   backgroundColor: '262834'
+  // },
+  // principalBox_on_Focus: {
+  //   backgroundColor: '383945'
+  // }
