375aef313a343d2dcde34e400aab57ee71f4b600
Gustavo Rodrigues <105459420+gutorafael@users.noreply.github.com>
2025-04-10 20:48:48 -0300

    FEAT(database)firebaseConfigs and getlists query (#22)

---
+++ database/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import Constants from 'expo-constants';
+
+export const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig.extra.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig.extra.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig.extra.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig.extra.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig.extra.FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
